25df954409b1c4b55e6c75c24b4b919703c1c240
Jasper Ras <jaspert.ras@gmail.com>
2022-10-04 13:02:40 +0200

    Initial pyenv plugin

---
+++ pyenv.plugin.zsh
@@ -0,0 +1,20 @@
+GITHUB="https://github.com"
+
+[[ -z "${PYENV_ROOT}" ]] && export PYENV_HOME="${HOME}/.pyenv"
+
+_zsh_pyenv_install() {
+    echo "Installing pyenv..."
+    git clone "${GITHUB}/pyenv/pyenv.git" "${PYENV_ROOT}"
+}
+
+_zsh_pyenv_load() {
+    export PATH="${PYENV_ROOT}/bin:${PATH}"
+    eval "$(pyenv init -)"
+}
+
+# First check if installed, if not, install.
+[[ ! -d "${PYENV_ROOT}" ]] && _zsh_pyenv_install
+
+# Then always check again to load, otherwise it may not load upon first install.
+[[ -d "${PYENV_ROOT}" ]] && _zsh_pyenv_load
+
